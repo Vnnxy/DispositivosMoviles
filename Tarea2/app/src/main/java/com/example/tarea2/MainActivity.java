054a9a65728b548348d51d88e0bdf05bb933358a
Miguel Akira López Asano <118841701+Vnnxy@users.noreply.github.com>
2024-10-22 10:34:33 -0600

    Tarea 2 diseño

--- Tarea2/app/src/main/java/com/example/tarea2/MainActivity.java
+++ Tarea2/app/src/main/java/com/example/tarea2/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.tarea2;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
 
+import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.receta);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.receta);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        } else {
+            // Log or handle the null case if needed
+        }
+
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menuSeleccion), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -24,4 +37,37 @@ public class MainActivity extends AppCompatActivity {
         });
          */
     }
-}
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.action_bar_home,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int i = R.id.settings;
+        int b = R.id.share;
+        int c = R.id.favorites;
+        int d = R.id.search;
+        int id = item.getItemId();
+        if(id == i){
+            Log.d("Se presionó:",(String) "settings");
+            return true;
+        } else if(id == b){
+            Log.d("Se presionó:",(String) "share");
+            return true;
+        } else if(id == c){
+            Log.d("Se presionó:",(String) "favorites");
+            return true;
+        } else if(id == d){
+            Log.d("Se presionó:",(String) "search");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+} 
